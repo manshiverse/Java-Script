47eabbc712c6aad5a6301859c3042bce8a3b48f3
manshiverse <manshipaswan56@gmail.com>
2026-08-14 15:26:13 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 8132737873479367364276327n
+
+
+
+// Refrence (Non - Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "spiderman", "nagraj"]; //Array
+let myObj = {
+    name: "manshi",
+    age: 18,
+} // Objects
+
+const myFunction = function (){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof heros );
